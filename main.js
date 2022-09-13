5db05b9bf04c4cfbb91a7e0b279f2bf5615805b1
siva <sivashakthiarun.c@gmail.com>
2022-09-13 10:20:12 +0530

    repush

---
+++ main.js
@@ -0,0 +1,7 @@
+var fabevy=["Don","Shakthi","Ajay","Wikki"];
+console.log("Original array:",fabevy);
+console.log("Length of original array:",fabevy.length);
+var a=fabevy.includes("Shakthi",2);
+console.log("Method:fabevy.includes('Shakthi');");
+console.log("Change in array:",a);
+console.log("Length of array after includes(); is ",a.length);
